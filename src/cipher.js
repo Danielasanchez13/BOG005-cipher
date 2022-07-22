21be0d1c9a811f552a481f0d54f6b1e5ce81edf3
Danielasanchez13 <danielasanchezlinares13@gmail.com>
2022-07-21 21:17:11 -0500

    version5

--- src/cipher.js
+++ src/cipher.js
@@ -2,5 +2,36 @@ window.addEventListener("load",inicio,true);
 function inicio(){
 	document.getElementById("mensaje").addEventListener("keyup",function(){
 		this.value = this.value.toUpperCase();
-	})
+	},true);
+	
+	document.getElementById("cifrar").addEventListener("click",function(){
+		let texto = document.getElementById("mensaje").value;
+		let desplazamiento = document.getElementById("desplazamiento").value;
+		document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
+	},true);
+	
+	document.getElementById("descifrar").addEventListener("click",function(){
+		let texto = document.getElementById("mensaje").value;
+		let desplazamiento = document.getElementById("desplazamiento").value;
+		 document.getElementById("mensaje2").value = descifrar(texto, desplazamiento);
+	},true);
+}
+
+function cifrar(texto, desplazamiento){
+	let resultado ="";
+	let letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
+	 
+	desplazamiento = (desplazamiento %26 + 26) %26;
+	if (texto){
+		for(let i = 0; i<texto.lengt; i++){
+			if (letras.indexOf(texto[i])!=-1){
+				let posicion = ((letras.indexOf(texto[i])+desplazamiento)%26);
+				resultado += letras[posicion];
+			}
+			else 
+			 resultado += texto[i];
+                    }
+    
+          } 
+          return resultado;
 }
