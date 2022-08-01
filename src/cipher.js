bfc142323d2a5e8bd5eb694ceddfd689f22afc98
Danielasanchez13 <danielasanchezlinares13@gmail.com>
2022-08-01 11:01:48 -0500

    version8

--- src/cipher.js
+++ src/cipher.js
@@ -14,89 +14,32 @@ function inicio(){
 	document.getElementById("descifrar").addEventListener("click", function(){
 		let texto = document.getElementById("mensaje").value;
 		let desplazamiento = document.getElementById("desplazamiento").value;
-		 document.getElementById("mensaje2").value = descifrar(texto, desplazamiento);
+		document.getElementById("mensaje2").value = descifrar(texto, desplazamiento);
 	},true);
 }
 
 function cifrar(texto, desplazamiento){
 	let resultado = "";
-	const letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
-	 
-	desplazamiento = (desplazamiento % 27 + 27) % 27;
+	let letras= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	desplazamiento = (desplazamiento % 26 + 26) % 26;
 	if (texto){
 		for(let i = 0; i<texto.length; i++){
 			if (letras.indexOf(texto[i])!=-1){
-				let posicion = ((letras.indexOf(texto[i])+desplazamiento)%27);
+				let posicion = ((letras.indexOf(texto[i])+desplazamiento)%26);
 				resultado += letras[posicion];
-			 }
+			}
 			else 
-			 resultado += texto[i];
+			resultado += texto[i];
                     }
     
           } 
   return resultado;
 }
-function cifrar2(texto, desplazamiento){
-	if(!texto)
-	   return "";
-	const letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
-	desplazamiento = (desplazamiento % 27 + 27) % 27;
-	return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento) % 27]); 
-}
 
 function descifrar(texto, desplazamiento){
 	if(!texto)
-	   return "";
-	const letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
-	desplazamiento = (desplazamiento % 27 - 27) % 27;
-	return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento) % 27]); 
-}document.getElementById("mensaje").addEventListener("keyup",function(){
-	this.value = this.value.toUpperCase();
-},true);
-
-document.getElementById("cifrar").addEventListener("click", function(){
-	let texto = document.getElementById("mensaje").value;
-	let desplazamiento = document.getElementById("desplazamiento").value;
-	document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
-},true);
-
-document.getElementById("descifrar").addEventListener("click", function(){
-	let texto = document.getElementById("mensaje").value;
-	let desplazamiento = document.getElementById("desplazamiento").value;
-	 document.getElementById("mensaje2").value = descifrar(texto, desplazamiento);
-},true);
-
-
-function cifrar(texto, desplazamiento){
-let resultado = "";
-let letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
- 
-desplazamiento = (desplazamiento % 27 + 27) % 27;
-if (texto){
-	for(let i = 0; i<texto.length; i++){
-		if (letras.indexOf(texto[i])!=-1){
-			let posicion = ((letras.indexOf(texto[i])+desplazamiento)%27);
-			resultado += letras[posicion];
-		 }
-		else 
-		 resultado += texto[i];
-	}
-
-} 
-return resultado;
-}
-function cifrar2(texto, desplazamiento){
-if(!texto)
-   return "";
-const letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
-desplazamiento = (desplazamiento % 27 + 27) % 27;
-return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento) % 27]); 
-}
-
-function descifrar(texto, desplazamiento){
-if(!texto)
-   return "";
-const letras= "ABCDEFGHIJKLMNÑOPQRSTUWXYZ";
-desplazamiento = (desplazamiento % 27 - 27) % 27;
-return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento) % 27]); 
+	return "";
+	const letras= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	desplazamiento = (desplazamiento % 26 - 26) % 26;
+	return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento) % 26]); 
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 
-function ingresar(){
-    var name = document.getElementById("nombreAgente").value;
-
-    if (name ==""){
-        alert("ingrese el nombre del agente");
-    } else {
+document.getElementById("ingresarNombre").addEventListener("click", function(){
+ let name = document.getElementById("nombreAgente").value;
+ if (name ==""){
+	alert("ingrese el nombre del agente");
+} else {
         document.getElementsByClassName("page2")[0].style.display="block";
-        var mensaje= "BIENVENID@ AGENTE " + name;
+        let mensaje= "BIENVENID@ AGENTE " + name;
         document.getElementById("saludo").innerHTML=mensaje; 
         document.getElementsByClassName("page1")[0].style.display="none";  
     }  
-}
+} );
 
-function inicio(){
-	document.getElementById("mensaje").addEventListener("keyup",function(){
+document.getElementById("mensaje").addEventListener("keyup",function(){
 		this.value = this.value.toUpperCase();
 	})
-}
+
 
 
